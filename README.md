2468e5f6044f550a271e18799c2ccb1a4561a9a4
sasha <alivak@proton.me>
2023-10-02 15:53:04 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Simple task manager
+
+### Functions
+You can create new entries and view them in a fast and convinient way.
+
+### Screenshots
+
+![Screenshot](screenshots/about.png)
+![Screenshot](screenshots/main_page.png)
+![Screenshot](screenshots/create.png)
+
+### Frameworks
+
+Built with django and bootstap. 
+
+<small>pika.style to style the screenshots.</small> 
---
+++ main/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+def index(request):
+    tasks = Task.objects.order_by('-id')[:5]
+    return render(request, "main/index.html", {'title': 'Main page', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, "main/about.html")
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Form is incorrect'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, "main/create.html", context)
